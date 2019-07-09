317894d41ef2a99c8e32605a5f747b16b666320e
Pablo Lopes <>
2019-07-09 16:48:32 -0300

    Created env files and config to get information for env Created db service to connect and disconnect on db Created test for db service

--- api/src/index.js
+++ api/src/index.js
@@ -4,13 +4,38 @@
 const express = require('express');
 const server = express();
 
-// listener to get requests for /
-server.get('/', (req, res) => {
-    res.send('Autocomplete API is working!!!');
-});
+// Body parser config
+const bodyParser = require('body-parser');
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
-// start server on the port defined by env
-server.app = server.listen(3000, () => {
+// Get config variables
+const config = require('./config');
 
-    console.log(`Server listening on port 3000`);
-});
+// Get database
+const { DBConnect } = require('./services/db.service');
+
+DBConnect().then(
+    () => {
+        /*** the server is only started if connected successfully on database ***/
+
+        // listener to get requests for /
+        server.get('/', (req, res) => {
+            res.send('Autocomplete API is working!!!');
+        });
+
+        // start server on the port defined by env
+        server.app = server.listen(config.portApi, () => {
+            // in dev, emit an event to be catch in integration tests
+            if (config.env === 'dev') {
+                server.emit('server-started');
+            }
+            console.log(`Server listening on port ${config.portApi}`);
+        });
+    },
+    (err) => {
+        console.log(err);
+    }
+);
+
+module.exports = server;
