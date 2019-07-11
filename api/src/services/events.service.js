aba8eb272123f6620940928dace99576b8b79cf1
Pablo Lopes <>
2019-07-11 14:11:39 -0300

    Added new method on event service, to get events on database Added tests for this new method

--- api/src/services/events.service.js
+++ api/src/services/events.service.js
@@ -6,7 +6,7 @@ const Events = require('../models/events.model');
 /**
  * Save an event and return the saved data
  * @param {Object} event object supposed to have event (String) and date (Date)
- * @throws {Error}
+ * @throws {Error} required-event | generic-insert
  * @returns {Object|MongooseSchema}
  */
 const SaveEvent = async (event) => {
@@ -22,4 +22,28 @@ const SaveEvent = async (event) => {
     }
 }
 
-module.exports = { SaveEvent };
+/**
+ * Get a list of events using the event name
+ * @param {String} name event name to find
+ * @throws generic-get
+ * @returns {Array} array of events objects
+ */
+const GetEventsByEvent = async (name) => {
+    try {
+        let events = await Events.find({ event: new RegExp(name) }).lean();
+
+        if (events.length > 0) {
+            return events.sort((a, b) => {
+                if (a.event.toLowerCase() > b.event.toLowerCase()) return 1;
+                else if (a.event.toLowerCase() < b.event.toLowerCase()) return -1;
+                else return 0;
+            });
+        } else {
+            return [];
+        }
+    } catch (err) {
+        throw new Error('generic-get');
+    }
+}
+
+module.exports = { SaveEvent, GetEventsByEvent };
--- api/test/services/events.service.test.js
+++ api/test/services/events.service.test.js
@@ -1,5 +1,5 @@
 const should = require('should'); // eslint-disable-line
-const { SaveEvent } = require('../../src/services/events.service');
+const { SaveEvent, GetEventsByEvent } = require('../../src/services/events.service');
 const Events = require('../../src/models/events.model');
 const { DBConnect, DBCloseConnection } = require('../../src/services/db.service');
 
@@ -15,7 +15,7 @@ describe('Events Service Test', () => {
         after(async () => {
             await Events.deleteMany({ event: "eventName" });
         });
-        it(`Should give an error if pass an event with wrong event attribute`, async () => {
+        it(`Should get an error if pass an event with wrong event attribute`, async () => {
             const event = { event: null, date: new Date("2019-07-09T20:03:42.780Z") };
             try {
                 await SaveEvent(event);
@@ -32,6 +32,61 @@ describe('Events Service Test', () => {
             const eventSaved = await Events.findOne({ event: "eventName" });
             eventSaved.should.have.properties(['_id', 'event', 'date']);
             eventSaved.event.should.be.equal('eventName');
+        });
+    });
+    describe('Get Events By Event', () => {
+        before(async () => {
+            // create events to be used in tests
+            await Events.create({ event: 'buy', date: '2016-09-22T13:57:31.2311892-04:00' });
+            await Events.create({ event: 'build', date: '2016-09-22T14:57:31.2311892-04:00' });
+            await Events.create({ event: 'burn', date: '2016-09-22T15:57:31.2311892-04:00' });
+            await Events.create({ event: 'sale', date: '2016-09-23T13:57:31.2311892-04:00' });
+            await Events.create({ event: 'salt', date: '2016-09-23T14:57:31.2311892-04:00' });
+            await Events.create({ event: 'say', date: '2016-09-23T15:57:31.2311892-04:00' });
+            await Events.create({ event: 'sit', date: '2016-09-23T16:57:31.2311892-04:00' });
+            await Events.create({ event: 'run', date: '2016-09-24T13:57:31.2311892-04:00' });
+        });
+        after(async () => {
+            await Events.deleteMany({ event: "buy" });
+            await Events.deleteMany({ event: "build" });
+            await Events.deleteMany({ event: "salt" });
+            await Events.deleteMany({ event: "burn" });
+            await Events.deleteMany({ event: "say" });
+            await Events.deleteMany({ event: "run" });
+            await Events.deleteMany({ event: "sit" });
+            await Events.deleteMany({ event: "sale" });
+        });
+        it(`Should get empty if string passed doesn't match with any event`, async () => {
+            const string = 'x';
+            const result = await GetEventsByEvent(string);
+            result.should.have.length(0);
+        });
+        it(`Should get one item if string passed match with one event`, async () => {
+            const string = 'ru';
+            const result = await GetEventsByEvent(string);
+            result.should.have.length(1);
+            result[0].should.have.property('event').and.be.match(new RegExp(string));
+        });
+        it(`Should get two ordered items if string passed match with two events`, async () => {
+            const string = 'al';
+            const result = await GetEventsByEvent(string);
+            result.should.have.length(2);
+            result[0].should.have.property('event').and.be.equal('sale');
+            result[1].should.have.property('event').and.be.equal('salt');
+        });
+        it(`Should get three ordered items if string passed match with three events`, async () => {
+            const string = 'bu';
+            const result = await GetEventsByEvent(string);
+            result.should.have.length(3);
+            result[0].should.have.property('event').and.be.equal('build');
+            result[1].should.have.property('event').and.be.equal('burn');
+            result[2].should.have.property('event').and.be.equal('buy');
+        });
+        it(`Should get one item if string passed match exactly with one event`, async () => {
+            const string = 'buy';
+            const result = await GetEventsByEvent(string);
+            result.should.have.length(1);
+            result[0].should.have.property('event').and.be.equal(string);
         });
     });
 });
